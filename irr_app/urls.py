35a8dd470a1c4f1ba72c76b3502bba8473824fa4
Tamkin Tamrazli <tamkintamraz@gmail.com>
2023-09-26 01:03:33 +0400

    login view

--- irr_app/urls.py
+++ irr_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from irr_app.views import HomePageView
+from irr_app.views import HomePageView, UserLoginView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home-page'),
+    path('login', UserLoginView.as_view(), name='user-login'),
 ]
--- irr_app/views.py
+++ irr_app/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.views import generic
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
 
 class HomePageView(generic.TemplateView):
     template_name = "irr_app/home_page.html"
+
+class UserLoginView(LoginView):
+    template_name = "irr_app/login.html"
+    authentication_form = AuthenticationForm
+
+
+
