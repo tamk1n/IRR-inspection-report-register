c97b49ed4eebd14ddcffa1fad583e85bec52e91a
Tamkin Tamrazli <tamkintamraz@gmail.com>
2023-09-29 00:47:23 +0400

    add new task button to home page

--- irr_app/urls.py
+++ irr_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from irr_app.views import HomePageView, UserLoginView
+from irr_app.views import (HomePageView, UserLoginView,
+                           NewIRView,)
 
 app_name = 'irr_app'
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home-page'),
     path('login', UserLoginView.as_view(), name='user-login'),
+    path('new', NewIRView.as_view(), name='new-ir'),
 ]
--- irr_app/views.py
+++ irr_app/views.py
@@ -3,16 +3,20 @@ from django.views import generic
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import AuthenticationForm
 from django.urls import reverse
-# Create your views here.
+
 
 class HomePageView(generic.TemplateView):
     template_name = "irr_app/home_page.html"
+
 
 class UserLoginView(LoginView):
     template_name = "irr_app/login.html"
     redirect_authenticated_user = False
     form_class = AuthenticationForm
 
+
+class NewIRView(generic.TemplateView):
+    pass
     
     
 
